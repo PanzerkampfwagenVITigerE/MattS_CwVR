4cc4f23f8362ff3e1100ca71fd19374b4059a45e
PanzerkampfwagenVITigerE <90841943+PanzerkampfwagenVITigerE@users.noreply.github.com>
2022-01-18 08:20:43 -0500

    16

---
+++ Assets/POITNS/Points.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points : MonoBehaviour
+{
+
+    public int points;
+    private GameObject triggeringObj;
+    private bool triggering;
+
+void Update()
+    {
+        if(triggering == true)
+        {
+            print("TEST");
+            if(input.)
+        
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+
+    }
+
+}
